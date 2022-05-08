1289306b4d4a0e41de0dad341231f6af826169fc
hotfur <75152429+hotfur@users.noreply.github.com>
2022-05-08 21:38:07 +0700

    Add test cases

--- test.py
+++ test.py
@@ -13,3 +13,14 @@ graph01 = ant_colony.Graph(matrix, 2, 10, 0.1, 3, 5)
 edges = ant_colony.ant_solver(graph01, 40)
 print(edges)
 
+#5 vertex and some edge not available
+matrix = np.array([[0., 7., 0., 15., 23.], [7., 0, 7., 5., 9.], [0., 7., 0., 2., 0.], [15., 5., 2., 0., 3.], [23., 9., 0., 3., 0.]])
+graph01 = ant_colony.Graph(matrix, 2, 1.1, 0.1, 3, 5)
+edges = ant_colony.ant_solver(graph01, 1000)
+print(edges)
+
+#5 verticles disconnected graph
+matrix = np.array([[0., 2., 0., 9., 0.], [2., 0, 0., 5., 0.], [0., 0., 0., 0., 3.], [9., 5., 0., 0., 0.], [0., 0., 3., 0., 0.]])
+graph01 = ant_colony.Graph(matrix, 2, 1.1, 0.1, 3, 5)
+edges = ant_colony.ant_solver(graph01, 1000)
+print(edges)
